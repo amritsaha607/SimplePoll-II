b75392a24cbf7c0816d20f745832340b9660526d
Amrit Saha <amritsaha607@gmail.com>
2022-03-04 16:58:02 +0530

    js authentication functions

---
+++ PollApp/static/auth.js
@@ -0,0 +1,42 @@
+function register() {
+  var sendData = {
+    "csrfmiddlewaretoken": csrf_token,
+    "mode": "register",
+    "username": $("#signUpModal #id_username").val(),
+    "password": $("#signUpModal #id_password").val(),
+  };
+  $.ajax({
+    url: authenticationURL,
+    type: 'POST',
+    data: sendData,
+    success: function (data) {
+      if (data.response == "ok") { window.location.reload(); }
+      else { alert(data.response); }
+    },
+    error: () => { alert("Something went wrong"); },
+    cache: false,
+  });
+}
+
+function login() {
+  var sendData = {
+    "csrfmiddlewaretoken": csrf_token,
+    "mode": "login",
+    "username": $("#loginModal #id_username").val(),
+    "password": $("#loginModal #id_password").val(),
+  };
+  $.ajax({
+    url: authenticationURL,
+    type: 'POST',
+    data: sendData,
+    success: function (data) {
+      if (data.response == "ok") { window.location.reload(); }
+      else { alert(data.response); }
+    },
+    error: () => { alert("Something went wrong"); },
+    cache: false,
+  });
+}
+
+$("#btn_register").on('click', register);
+$("#btn_login").on('click', login);
