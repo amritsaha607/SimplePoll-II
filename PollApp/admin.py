02ae735c01cbe79591f28124350d0ecbee9a1e64
Amrit Saha <amritsaha607@gmail.com>
2022-03-04 16:49:27 +0530

    added user in admin panel

--- PollApp/admin.py
+++ PollApp/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from PollApp.models import Choice, Poll, Vote
+from PollApp.models import Choice, Poll, User, Vote
 
 # Register your models here.
 
+admin.site.register(User)
 admin.site.register(Choice)
 admin.site.register(Poll)
 admin.site.register(Vote)
